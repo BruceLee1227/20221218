5682640e8ec8a56b320c42d8baf02eaf91f5b861
Bruce <bruce8046@gmail.com>
2023-03-19 11:19:08 +0800

    function 使用const(常數)宣告

--- dist/js/js_function.js
+++ dist/js/js_function.js
@@ -1,8 +1,13 @@
 // alert(12356);
 
-function add(n1,n2){
+// function add(n1,n2){
+//     return n1 + n2;
+// 
+
+const add= function(n1,n2){
     return n1 + n2;
 }
 
+// let add=565456465;
 let calc= add(10,30);
 console.log(calc);
