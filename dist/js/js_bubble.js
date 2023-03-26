e0d2ec842189df2d702ca5e342a86770475507a6
Bruce <bruce8046@gmail.com>
2023-03-26 15:27:51 +0800

    事件氣泡

---
+++ dist/js/js_bubble.js
@@ -0,0 +1,7 @@
+let add=document.querySelector('#add-btn');
+
+add.addEventListener('click',()=>{
+    let menu=document.querySelector('#menu');
+    let li=document.querySelector('li');
+    li.innerHTML="5";
+})
